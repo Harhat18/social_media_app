5fe9176fbb80617c2b7d5966ceb6a278fa244178
Harhat18 <harunhatib18@gmail.com>
2023-10-06 23:36:11 +0300

    add commit client connections axios

--- api/index.js
+++ api/index.js
@@ -2,27 +2,31 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
-import likeRoutes from './routes/likes.js'
-import commentRoutes from './routes/comments.js'
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
 
+const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
 
-
-const app = express();   
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-
-
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/posts', postRoutes)
-app.use('/api/likes', likeRoutes)
-app.use('/api/comments', commentRoutes)
-
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(8800, () => {
-    console.log('API working!');
+  console.log("API working!");
 });
