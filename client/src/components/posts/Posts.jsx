ac9cf2a925ad5b40f58a29fd64343a092e76c3e3
Harhat18 <harunhatib18@gmail.com>
2023-10-07 15:51:31 +0300

    Add commit created post

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,14 +1,23 @@
 import Post from "../post/Post";
 import "./posts.scss";
 
-const Posts = () => {
-  //TEMPORARY
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+
+const Posts = ({ userId }) => {
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    makeRequest.get("/posts").then((res) => {
+      return res.data;
+    })
+  );
 
   return (
     <div className="posts">
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
+      {error
+        ? "Something went wrong!"
+        : isLoading
+        ? "loading"
+        : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
 };
